c554170c01573b291f40bf27d1fa5f46f7ddb30f
Calewi12 <calewi12@asu.edu>
2025-03-19 22:43:33 -0700

    updates

--- main.py
+++ main.py
@@ -20,7 +20,13 @@ while True:
         print(c)
         # toggle the onboard LED
         led.value(led.value()^1)
-	# sleep for a very small amount of time
+        t2 = c.decode("utf-8')
+	    s=s+t2
+	    if t2=='\n':|
+            print(s)
+            s=''
+            
+            
     else:
         # uart.write(b'test\n')
         a=input('type something and hit enter: ')
